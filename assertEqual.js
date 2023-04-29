af0e6c592b1ce0969052c98aca51c9c483f095c3
Sandeep Gill <sandeepgill62@gmail.com>
2023-04-29 19:05:04 +0000

    create assertEqual test function using back-ticks

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,9 @@
 const assertEqual = function(actual, expected) {
 
   if(actual === expected)
-    console.log("🛑🛑🛑 Assertion Passed: " + actual + " === " + expected);
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
   else
-    console.log("✅✅✅ Assertion Failed: " + actual + " !== " + expected);
-
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
