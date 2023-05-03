92fd0e041572e1563241e889f4dcb34c21f95038
Sandeep Gill <sandeepgill62@gmail.com>
2023-05-03 05:32:39 +0000

    findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected)
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+const findKeyByValue = function (object, value) {
+  var result;
+  for (const key in object) {
+    if (object[key] === value)
+      result = key;
+  }
+  return result;
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
