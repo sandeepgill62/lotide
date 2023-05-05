dcdc06244ce14ea5d78115cf5de6c9dd1d4893c3
Sandeep Gill <sandeepgill62@gmail.com>
2023-05-05 04:19:17 +0000

    findKey function

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected)
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function (object, callback) {
+  var result;
+  for (const key in object) {
+    if (callback(object[key])) {
+      result = key;
+      return result;
+    }
+  }
+  return result;
+}
+
+
+assertEqual((findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2)), "noma");
+
